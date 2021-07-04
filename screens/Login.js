3ad493d60f4f1e4ce6bb6615696fbfecd29bfc1a
KlassiC-Man <parth.11.gulati@gmail.com>
2021-07-04 19:09:13 +0530

    Profile Images still not available!

--- screens/Login.js
+++ screens/Login.js
@@ -28,13 +28,17 @@ function Login({navigation}) {
     };
 
     return (
-        <KeyboardAvoidingView behavior='height' style={styles.container}>
-            <Input type='text' placeholder='Username' value={name} onChangeText={(text) => setName(text)} style={styles.input} />
-            <Input type='email' placeholder='E-Mail' value={email} onChangeText={(text) => setEmail(text)} style={styles.input} />
-            <Input type='password' placeholder='Password' secureTextEntry value={password} onChangeText={(text) => setPassword(text)} style={styles.input}/>
-            <Button title='Login' raised onPress={login} style={styles.button} />
-            <Button title='Sign Up' type='clear' onPress={signUp} style={styles.button} />
-        </KeyboardAvoidingView>
+        <View>
+            <View style={{backgroundColor: 'purple', height: 100, width: 200, borderBottomRightRadius: 60}}></View>
+            <View style={{backgroundColor: 'cadetblue', height: 50, width: 170, borderBottomRightRadius: 50}}></View>
+            <KeyboardAvoidingView behavior='height' style={styles.container}>
+                <Input type='text' placeholder='Username' value={name} onChangeText={(text) => setName(text)} style={styles.input} />
+                <Input type='email' placeholder='E-Mail' value={email} onChangeText={(text) => setEmail(text)} style={styles.input} />
+                <Input type='password' placeholder='Password' secureTextEntry value={password} onChangeText={(text) => setPassword(text)} style={styles.input}/>
+                <Button title='Login' raised onPress={login} style={styles.button} />
+                <Button title='Sign Up' type='clear' onPress={signUp} style={styles.button} />
+            </KeyboardAvoidingView>
+        </View>
 
     )
 }
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,44 +1,89 @@
-import React, { useState } from 'react';
-import {KeyboardAvoidingView, ScrollView, StyleSheet, View, TouchableOpacity, Text} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {KeyboardAvoidingView, ScrollView,Image , StyleSheet, View, TouchableOpacity, Text, TextInput, ImagePickerIOS} from 'react-native';
 import {Input, Button} from 'react-native-elements';
+import { Avatar } from 'react-native-elements/dist/avatar/Avatar';
 import {auth, db} from '../firebase';
+import * as ImagePicker from 'expo-image-picker';
 
 function SignUp({navigation}) {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [imageUrl, setImageUrl] = useState('');
+    const [imageUrl, setImageUrl] = useState('../assets/defaultPic.jpeg');
     const [bio, setBio] = useState('');
+
+    useEffect(() => {
+        (async () => {
+            if (Platform.OS !== 'web') {
+                const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync();
+                if (status !== 'granted') {
+                    alert('We need camera and Image Acces!!!');
+                }
+            }
+        })
+    }, []);
 
     function signUp() {
         auth.createUserWithEmailAndPassword(email, password).then(authUser => {
             authUser.user.updateProfile({
                 displayName: name,
-                photoURL: imageUrl || require('../assets/default.png'),
-            }).then(
-                db.collection('users').doc(name).set({
-                    name: name,
-                    email: email,
-                    followers: [],
-                    following: [],
-                    posts: [],
-                    profilePic: imageUrl,
-                })
-            )
+            })
+        })
+        const data = new FormData();
+        data.append('file', imageUrl);
+        data.append('upload_preset', 'pfrzfrnr');
+        data.append('cloud_name', 'dyin2a2pd');
+        fetch('https://api.cloudinary.com/v1_1/dyin2a2pd/image/upload/', {
+            method: 'post',
+            body: data
+        }).then(res => res.json()).then(data => {
+            db.collection('users').doc(name).set({
+                name: name,
+                email: email,
+                followers: [],
+                following: [],
+                posts: [],
+                profilePic: data.secure_url,
+            }) 
         }).catch(error => alert(error));
+    };
+        
+
+    async function pickProfile() {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [7, 10],
+            quality: 1,
+        })
+        console.log(result);
+
+        if (!result.cancelled) {
+            let newFile = {
+                uri: result.uri,
+                type: `test/${result.uri.split('.')[1]}`,
+                name: 'test.png',
+            }
+            setImageUrl(result.uri);
+        }
     };
 
     return (
-        <ScrollView>
-            <Input type='text' placeholder='Username' value={name} onChangeText={text => setName(text)} style={styles.input} />
-            <Input type='email' placeholder='E-Mail' value={email} onChangeText={text => setEmail(text)} style={styles.input} />
-            <Input type='password' placeholder='password' value={password} secureTextEntry onChangeText={text => setPassword(text)} style={styles.input} />
-            <Input type='text' placeholder='Image URL for Profile (Optional) .png or .jpeg links' onChangeText={text => setImageUrl(text)} style={styles.input} value={imageUrl} />
+        <ScrollView style={{color:'black'}}>
+            <View style={{backgroundColor: 'purple', height: 100, width: 200, borderBottomRightRadius: 60}}></View>
+            <View style={{backgroundColor: 'cadetblue', height: 50, width: 170, borderBottomRightRadius: 50}}></View>
+            <TouchableOpacity onPress={pickProfile} style={{marginTop: 5, flexDirection:'row', justifyContent: 'center'}}>
+                <Avatar source={{uri: imageUrl}} size={100} rounded />
+            </TouchableOpacity>
+            <TextInput type='text' placeholder='Username' value={name} onChangeText={text => setName(text)} style={styles.input} />
+            <TextInput type='email' placeholder='E-Mail' value={email} onChangeText={text => setEmail(text)} style={styles.input} />
+            <TextInput type='password' placeholder='password' value={password} secureTextEntry onChangeText={text => setPassword(text)} style={styles.input} />
+            {/*<TextInput type='text' placeholder='Image URL for Profile (Optional) .png or .jpeg links' onChangeText={text => setImageUrl(text)} style={styles.input} value={imageUrl} />*/}
             {/*<Input type='text' placeholder='Bio' value={bio} onChangeText={text => setBio(text)} style={{marginTop: 20, paddingTop: 10, height: 20}} multiline={true} />*/}
             {/*<Button title='Choose Profile Picture' onPress={profilePicChooser} style={styles.button} />*/}
             <Button title='Sign Up!' onPress={signUp} raised style={styles.button} />
             <TouchableOpacity>
-                <Text>Create An Organisation (Not functional As Of Now!)</Text>
+                {/*<Text>Create An Organisation (Not functional As Of Now!)</Text>*/}
             </TouchableOpacity>
         </ScrollView>
     )
@@ -49,7 +94,11 @@ export default SignUp;
 const styles = StyleSheet.create({
     input: {
         marginTop: 20,
-        paddingTop: 10
+        paddingTop: 10,
+        borderWidth: 1,
+        margin: 10,
+        height: 50,
+        borderRadius: 10
     },
     button: {
         marginTop: 50
